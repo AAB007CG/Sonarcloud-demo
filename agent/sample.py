96dc9eff0de215965eacd74a30567e8aa54eced0
AAB007CG <akash.benki@capgemini.com>
2026-01-28 17:16:41 +0530

    Introduced the Intentional Errors in the Sample.py file

--- agent/sample.py
+++ agent/sample.py
@@ -1,5 +1,5 @@
-# def insecure():
-#     username = "admin"
-#     password = "admin123" # password is exposed intentionally
-#     @123 # Intentional Syntax Error
-#     return eval("2 + 2")
+def insecure():
+    username = "admin"
+    password = "admin123" # password is exposed intentionally
+    @123 # Intentional Syntax Error
+    return eval("2 + 2")
