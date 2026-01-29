a959ebaba87f05f63a12ec2bd23b3ecbbdd7b9cc
AAB007CG <akash.benki@capgemini.com>
2026-01-29 12:14:26 +0530

    Testing Copilot Code Review

--- sample.py
+++ sample.py
@@ -2,5 +2,5 @@ def insecure():
     username = "admin"
     password = "admin123" # password is exposed intentionally
     @123 # Intentional Syntax Error
-    username = "admin2"
+    username = "admin2",
     return eval("2 + 2")
