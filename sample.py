b036d18d5a5732dfaf30f0b99ed7faa81372170c
AAB007CG <akash.benki@capgemini.com>
2026-01-29 15:05:18 +0530

    Modified sample.py file

--- sample.py
+++ sample.py
@@ -1,6 +1,4 @@
 def insecure():
     username = "admin"
     password = "admin123" # password is exposed intentionally
-    @123 # Intentional Syntax Error
-    username = "admin2", # Intentional Error
     return eval("2 + 2")
