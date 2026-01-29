833cd78a27a14d4139fceb951478f1073fb861cb
AAB007CG <akash.benki@capgemini.com>
2026-01-29 12:11:45 +0530

    Adding Intentional error in the sample.py file

--- sample.py
+++ sample.py
@@ -2,4 +2,5 @@ def insecure():
     username = "admin"
     password = "admin123" # password is exposed intentionally
     @123 # Intentional Syntax Error
+    username = "admin2"
     return eval("2 + 2")
