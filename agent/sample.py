deea6669db6ea6a06f66206fd382a80d2c1fe8e5
AAB007CG <akash.benki@capgemini.com>
2026-02-12 12:44:58 +0530

    Testing the Failure Case

--- agent/sample.py
+++ agent/sample.py
@@ -1,4 +1,5 @@
 def insecure():
     username = "admin"
     password = "admin123" # password is exposed intentionally
+    justtestinghere = "This is for Failure case testing only"
     return eval("2 + 2")
